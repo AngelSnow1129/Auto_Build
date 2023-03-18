df48034bee1ccd3ef32d139f7d65b96c858fed5e
Snow <82687007+AngelSnow1129@users.noreply.github.com>
2023-03-18 12:59:02 +0800

    Update Auto_Build.py

--- Auto_Build.py
+++ Auto_Build.py
@@ -22,11 +22,12 @@ def remove_upprintable_chars(s):
     """移除所有不可见字符"""
     return ''.join(x for x in s if x.isprintable())
 
-url = remove_upprintable_chars(url)
+
 # url=url.isprintable()
 
-print(url)
+# print(url)
 url=text.format(scriptID=sI)
+url = remove_upprintable_chars(url)
 data='''{{"robots": [{{ "_id": "{0}", "name": "{1}"}}]}}'''.format(bI,bN)
 headers_str='''{{"Authorization":"{0}","Content-Type":"application/json"}}'''.format(aT)
 
