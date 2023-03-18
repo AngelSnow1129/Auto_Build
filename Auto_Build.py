8d789a8b1e35638f9493530f33113870b587e1aa
Snow <82687007+AngelSnow1129@users.noreply.github.com>
2023-03-18 12:48:46 +0800

    Add files via upload

--- Auto_Build.py
+++ Auto_Build.py
@@ -7,6 +7,8 @@ import re
 import sys
 import requests
 import json
+import ast
+
 
 sI=os.environ.get('sI')
 bI=os.environ.get('bI')
@@ -14,10 +16,12 @@ bN=os.environ.get('bN')
 aT=os.environ.get('aT')
 text=os.environ.get('text')
 
+
 url=text.format(scriptID=sI)
 data='''{{"robots": [{{ "_id": "{0}", "name": "{1}"}}]}}'''.format(bI,bN)
-headers='''{{"Authorization":"{0}","Content-Type":"application/json"}}'''.format(aT)
+headers_str='''{{"Authorization":"{0}","Content-Type":"application/json"}}'''.format(aT)
 
+headers=ast.literal_eval(headers_str)
 
 result = requests.post(url, data=json.dumps(data) , headers=headers)
 # print(result)
